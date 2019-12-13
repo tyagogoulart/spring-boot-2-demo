003bff84824af2635cd67a982830df42358e5854
Tyago Goulart <tyago.goulart@iMac-de-Tyago.local>
2019-12-13 17:02:23 -0300

    Adding PUT method and refactoring find

--- src/main/java/com/goulart/market/resources/CategoriaResource.java
+++ src/main/java/com/goulart/market/resources/CategoriaResource.java
@@ -1,8 +1,6 @@
 package com.goulart.market.resources;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 
 import com.goulart.market.domain.Categoria;
 import com.goulart.market.services.CategoriaService;
@@ -25,7 +23,7 @@ public class CategoriaResource {
 
     @RequestMapping(value="/{id}", method=RequestMethod.GET)
     public ResponseEntity<?> find(@PathVariable Integer id) {
-        Categoria obj = service.buscar(id);
+        Categoria obj = service.find(id);
         return ResponseEntity.ok().body(obj);
     }
 
@@ -35,5 +33,12 @@ public class CategoriaResource {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(value="/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
     }
 }
