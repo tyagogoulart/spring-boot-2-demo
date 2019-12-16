ffba499d1f52282859cef84066d1770961a7dafd
Tyago Goulart <tyago.goulart@iMac-de-Tyago.local>
2019-12-16 13:18:17 -0300

    Custom validation message

---
+++ src/main/java/com/goulart/market/resources/exceptions/FieldMessage.java
@@ -0,0 +1,39 @@
+package com.goulart.market.resources.exceptions;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String fieldName;
+    private String message;
+
+
+    public FieldMessage() {
+    }
+
+    public FieldMessage(String fieldName, String message) {
+        super();
+        this.fieldName = fieldName;
+        this.message = message;
+    }
+
+    public String getFieldName() {
+        return this.fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    
+}
