ada7b2cf01eabee853985ab63400b5395b711005
Tyago Goulart <tyago.goulart@iMac-de-Tyago.local>
2019-12-16 10:03:29 -0300

    Pagination with Page and RequestParams

--- src/main/java/com/goulart/market/services/CategoriaService.java
+++ src/main/java/com/goulart/market/services/CategoriaService.java
@@ -10,6 +10,9 @@ import com.goulart.market.services.exceptions.ObjectNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 
@@ -45,5 +48,10 @@ public class CategoriaService {
     
     public List<Categoria> findAll() {  
         return repo.findAll();
+    }
+
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+        return repo.findAll(pageRequest);
     }
 }
